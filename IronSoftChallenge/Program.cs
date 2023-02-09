d51ed23235b869012c15f1cfd13c2020687f1a90
larkin <larkincrain@gmail.com>
2023-02-09 08:06:02 +0700

    Added error checking surrounding input in the program.cs

--- IronSoftChallenge/Program.cs
+++ IronSoftChallenge/Program.cs
@@ -2,5 +2,11 @@
 
 Console.WriteLine("Please enter the T9 input: ");
 string input = Console.ReadLine();
+
+if (String.IsNullOrEmpty(input))
+{
+    Console.WriteLine("Input is null");
+    return;
+}
 string text = T9Converter.OldPhonePad(input);
 Console.WriteLine(text);
